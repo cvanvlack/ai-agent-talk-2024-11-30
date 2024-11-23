a9f191d6d95dc23a8094eef4b5d447c25c61a4de
Cole Van Vlack <cvanvlack@gmail.com>
2024-11-23 10:53:30 -0500

    Switched agent to use Tavily

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import getpass
-import os
-
 from dotenv import load_dotenv
-load_dotenv()
-
+from langchain_core.messages import HumanMessage
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_openai import ChatOpenAI
 
+load_dotenv()
+
+
+
 model = ChatOpenAI(model="gpt-4")
+search = TavilySearchResults(max_results=2)
+# If we want, we can create other tools.
+# Once we have all the tools we want, we can put them in a list that we will reference later.
+tools = [search]
 
-from langchain_core.messages import HumanMessage
-
-response = model.invoke([HumanMessage(content="hi!")])
-print(response.content)
+from langgraph.prebuilt import create_react_agent
+agent_executor = create_react_agent(model, tools)
+response = agent_executor.invoke(
+    {"messages": [HumanMessage(content="whats the weather in sf?")]}
+)
+print(response)
+print(response["messages"])
