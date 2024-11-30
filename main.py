ead2e82f5672dcb0b4dbb6fdce693877e8749df9
Cole Van Vlack <cvanvlack@gmail.com>
2024-11-30 18:02:37 -0500

    Got it to work

--- main.py
+++ main.py
@@ -1,69 +1,54 @@
 from dotenv import load_dotenv
-
-from typing import Annotated
-from typing_extensions import TypedDict
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-from langchain_community.tools.tavily_search import TavilySearchResults
-
-from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.memory import MemorySaver
+from tavily import TavilyClient
+from langchain_openai import ChatOpenAI
+from agent_state import AgentState
 
-
+from nodes import plan_node, generation_node, reflection_node, research_plan_node, research_critique_node, should_continue
 
 # Load the environment variables
 load_dotenv()
 
-class State(TypedDict):
-    # Messages have the type "list". The `add_messages` function
-    # in the annotation defines how this state key should be updated
-    # (in this case, it appends messages to the list, rather than overwriting them)
-    messages: Annotated[list, add_messages]
+tavily = TavilyClient()
 
-
-graph_builder = StateGraph(State)
-
-from langchain_openai import ChatOpenAI
-
-model = ChatOpenAI(model="gpt-4")
-
-def chatbot(state: State):
-    return {"messages": [model.invoke(state["messages"])]}
-
-# The first argument is the unique node name
-# The second argument is the function or object that will be called whenever
-# the node is used.
-graph_builder.add_node("chatbot", chatbot)
-
-# Next, add an entry point. This tells our graph where to start its work each time we run it.
-graph_builder.add_edge(START, "chatbot")
-
-# Similarly, set a finish point. This instructs the graph "any time this node is run, you can exit."
-graph_builder.add_edge("chatbot", END)
-
+model = ChatOpenAI(model="gpt-4o-mini")
 memory = MemorySaver()
-graph = graph_builder.compile(
-    checkpointer=memory,
-    interrupt_before=["tools"]
-    )
 
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+builder = StateGraph(AgentState)
+initial_state: AgentState = {
+    "task": "what is the difference between langchain and langsmith", 
+    "plan": None,
+    "draft": None,
+    "critique": None,
+    "content": [],
+    "max_revisions": 2, 
+    "revision_number": 1,
+}
 
+builder.add_node("planner", lambda state: plan_node(state, model))
+builder.add_node("research_plan", lambda state: research_plan_node(state, model, tavily))
+builder.add_node("generate", lambda state: generation_node(state, model))
+builder.add_node("reflect", lambda state: reflection_node(state, model))
+builder.add_node("research_critique", lambda state: research_critique_node(state, model, tavily))
 
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
+builder.add_conditional_edges(
+    "generate", 
+    should_continue, 
+    {END: END, "reflect": "reflect"}
+)
 
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+builder.set_entry_point("planner")
+builder.add_edge("planner", "research_plan")
+builder.add_edge("research_plan", "generate")
+
+builder.add_edge("reflect", "research_critique")
+builder.add_edge("research_critique", "generate")
+
+graph = builder.compile(checkpointer=memory)
+
+thread = {"configurable": {"thread_id": "1"}}
+   
+
+for s in graph.stream(initial_state, thread):
+    print(s)
---
+++ nodes.py
@@ -0,0 +1,70 @@
+from langchain_core.messages import SystemMessage, HumanMessage
+from prompts import PLAN_PROMPT, RESEARCH_PLAN_PROMPT, RESEARCH_CRITIQUE_PROMPT, WRITER_PROMPT, REFLECTION_PROMPT
+from agent_state import AgentState
+from queries import Queries
+from tavily import TavilyClient
+from pydantic import BaseModel
+from langgraph.graph import END
+
+def plan_node(state: AgentState, model: BaseModel):
+    messages = [
+        SystemMessage(content=PLAN_PROMPT), 
+        HumanMessage(content=state['task'])
+    ]
+    response = model.invoke(messages)
+    return {"plan": response.content}
+
+def research_plan_node(state: AgentState, model: BaseModel, tavily: TavilyClient):
+    queries = model.with_structured_output(Queries).invoke([
+        SystemMessage(content=RESEARCH_PLAN_PROMPT),
+        HumanMessage(content=state['task'])
+    ])
+    print(queries)
+    print(state)
+    content = state['content'] or []
+    for q in queries.queries:
+        response = tavily.search(query=q, max_results=2)
+        for r in response['results']:
+            content.append(r['content'])
+    return {"content": content}
+
+def generation_node(state: AgentState, model: BaseModel):
+    content = "\n\n".join(state['content'] or [])
+    user_message = HumanMessage(
+        content=f"{state['task']}\n\nHere is my plan:\n\n{state['plan']}")
+    messages = [
+        SystemMessage(
+            content=WRITER_PROMPT.format(content=content)
+        ),
+        user_message
+        ]
+    response = model.invoke(messages)
+    return {
+        "draft": response.content, 
+        "revision_number": state.get("revision_number", 1) + 1
+    }
+
+def reflection_node(state: AgentState, model: BaseModel):
+    messages = [
+        SystemMessage(content=REFLECTION_PROMPT), 
+        HumanMessage(content=state['draft'])
+    ]
+    response = model.invoke(messages)
+    return {"critique": response.content}
+
+def research_critique_node(state: AgentState, model: BaseModel, tavily: TavilyClient):
+    queries = model.with_structured_output(Queries).invoke([
+        SystemMessage(content=RESEARCH_CRITIQUE_PROMPT),
+        HumanMessage(content=state['critique'])
+    ])
+    content = state['content'] or []
+    for q in queries.queries:
+        response = tavily.search(query=q, max_results=2)
+        for r in response['results']:
+            content.append(r['content'])
+    return {"content": content}
+
+def should_continue(state: AgentState):
+    if state["revision_number"] > state["max_revisions"]:
+        return END
+    return "reflect"
